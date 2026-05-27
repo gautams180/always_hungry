48aad218696b6eff950144af8275af734d97a637
gautams180 <sahugautam944@gmail.com>
2026-05-27 22:05:57 +0530

    db config changes

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,16 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
 connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
+    host=MYSQL_HOST,
+    user=MYSQL_USER,
+    password=MYSQL_PASSWORD,
     database="always_hungry"
 )
 
